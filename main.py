9f07f678e6513ce5c1681d16659011182ca88dd4
Bruno Rodrigo <bnorodrigo@gmail.com>
2024-02-12 08:35:45 +0000

    Database setup

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from database import models
+from database.database import engine
 
 app = FastAPI()
 
 @app.get("/")
 def hw():
     return "Hello world!"
+
+models.Base.metadata.create_all(engine)
