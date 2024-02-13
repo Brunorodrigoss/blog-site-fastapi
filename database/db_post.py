c83dab96526313a618a133f99d2a3bfd53d505a1
Bruno Rodrigo <bnorodrigo@gmail.com>
2024-02-13 08:19:04 +0000

    Delete post

--- database/db_post.py
+++ database/db_post.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException, status
 from routers.schemas import PostBase
 from sqlalchemy.orm.session import Session
 from database.models import DbPost
@@ -20,3 +21,17 @@ def create(db: Session, request: PostBase):
 
 def get_all(db: Session):
     return db.query(DbPost).all()
+
+def delete(id: int, db: Session):
+    post = db.query(DbPost).filter(DbPost.id == id).first()
+
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'Post with id {id} not found'
+        )
+
+    db.delete(post)
+    db.commit()
+
+    return 'Ok'
